9fcc0feac0cf0f64e8beaea173b42b9e3c83de57
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-02-11 15:25:18 -1000

    First gallery GET route created

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-var express = require('express');
+const express = require('express');
+const gallery = require('./routes/gallery');
 const handlebars = require('express-handlebars');
-var app = express();
-var bp = require('body-parser');
+const app = express();
+const bp = require('body-parser');
 
-var db = require('./models');
+const db = require('./models');
 
 //bodyparser
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.use('/gallery', gallery);
 
 //handlebars
 const hbs = handlebars.create({
@@ -27,3 +30,5 @@ app.listen(3000, function() {
   console.log('Listening on 3000');
   db.sequelize.sync();
 });
+
+module.exports = app;
