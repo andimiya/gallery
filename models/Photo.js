36c5edc82f44234f811bb152fcb23dae8edbd438
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-02-15 23:05:53 -1000

    Added more styling to the buttons

--- models/Photo.js
+++ models/Photo.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Photo = sequelize.define("Photo", {
     author: DataTypes.STRING,
     link: DataTypes.STRING,
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
   }, {
     classMethods: {
       associate: function(models) {
